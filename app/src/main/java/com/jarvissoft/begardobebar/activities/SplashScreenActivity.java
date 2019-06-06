4d9ab3f7960e6dd11b57923abd528e0696444e01
Pooyan Zandian <pooyan.zandiyan76@gmail.com>
2019-06-06 16:35:07 +0430

    change something...

--- app/src/main/java/com/jarvissoft/begardobebar/activities/SplashScreenActivity.java
+++ app/src/main/java/com/jarvissoft/begardobebar/activities/SplashScreenActivity.java
@@ -3,19 +3,22 @@ package com.jarvissoft.begardobebar.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
 import com.jarvissoft.begardobebar.R;
 import com.jarvissoft.begardobebar.utils.pref.SystemPrefs;
+import com.squareup.picasso.Picasso;
 
 public class SplashScreenActivity extends MyBaseActivity {
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+		
 		new Thread() {
 			public void run() {
 				try {
-					sleep(1000);
+					sleep(4000);
 				} catch (InterruptedException e) {
 					e.getStackTrace();
 				} finally {
